58271612e3467f9d117efdcc51d2777546ada775
Aman Prasad <amanpd14150518@gmail.com>
2025-12-10 09:17:04 +0530

    updated code

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+let data = require("./ProblemSet.json");
+
+
+
+app.listen(()=>{
+    console.log("app is listening at port 3000");
+})
+
